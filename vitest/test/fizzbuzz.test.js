bfe41337d4db4746b9014fe71265f61dbeba6095
Simon <simontorbett@gmail.com>
2024-10-03 22:01:22 -0300

    .

--- vitest/test/fizzbuzz.test.js
+++ vitest/test/fizzbuzz.test.js
@@ -1,19 +1,18 @@
-import {describe} from 'vitest'
+import { describe, it, expect } from 'vitest';
 
-
-
-const fizzbuzz  ()  => {
-
+const fizzbuzz = (number) : void => {
+    if (typeof number !== 'number') throw new Error('parameter provided must be a number')
+    
 }
 
-
-describe('fizzbuzz', ()  => {
-    it('should be a function', ()  => {
+describe('fizzbuzz', () : void => {
+    it('should be a function', () : void => {
         expect(typeof fizzbuzz).toBe('function');
-        
+    });
+    it('should throw if not number is provided as parameter', () : void => {
+        expect (fizzbuzz()).toThrow()
 
-})
-
-
-
-})
+    })
+    it('should throw a specific error message if not number is provider as parameter', () : void => {
+        expect (() : void => fizzbuzz()).toThrow(/number/)
+});
