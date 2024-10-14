d8d1aac7126220cbe839b3e5d3fe3ba5ea49b355
Simon <simontorbett@gmail.com>
2024-10-14 19:51:27 -0300

    .

--- vitest/test/can-configure.test.ts
+++ vitest/test/can-configure.test.ts
@@ -1,20 +1,26 @@
 import { describe, expect, it } from "vitest";
 
+const canConfigure = (from, to): void => {
+  if (typeof from !== "string") throw new Error("from is not a string");
 
-const canConfigure = (from): void => {
-    if (from === undefined) throw new Error('from is required')
-    if (typeof from!=='string') throw new Error('from must be a string')
-}
-describe("canReconfigure", (): void => {
-    it('should be a fuction', (): void => {
-     expect(canConfigure).toBeTypeOf('function')
-    })
+  if (typeof to !== "string") throw new Error("to is not a string");
+};
 
-    it('should trow if first parameter is missing', (): void => {
-        expect((): void => canConfigure()).toThrow()
-    })
+describe("canConfigure", (): void => {  // Aquí cambiaste a "canConfigure"
+  it("should be a function", (): void => {
+    expect(canConfigure).toBeTypeOf("function");
+  });
 
-    it('should trow if first parameter is not a string', (): void => {
-        expect((): void => canConfigure(2)).toThrow()
-    })
-})
+  it("should throw if first parameter is missing", (): void => {
+    expect((): void => canConfigure()).toThrow();
+  });
+
+  it("should throw if first parameter is not a string", (): void => {
+    expect((): void => canConfigure(2)).toThrow();
+  });
+
+  it("should throw if second parameter is not a string", (): void => {
+    expect((): void => canConfigure("a")).toThrow();
+  });
+});
+
