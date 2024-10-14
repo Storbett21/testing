eefcdc9a2deb9640bc86c1a32a775210c4195788
Simon <simontorbett@gmail.com>
2024-10-14 19:58:40 -0300

    .

--- vitest/test/can-configure.test.ts
+++ vitest/test/can-configure.test.ts
@@ -1,26 +1,38 @@
 import { describe, expect, it } from "vitest";
 
-const canConfigure = (from, to): void => {
+const canReconfigure = (from, to): void => {
   if (typeof from !== "string") throw new Error("from is not a string");
 
   if (typeof to !== "string") throw new Error("to is not a string");
+
+  if (from.length !== to.length) return false
+ 
+  return true
 };
 
-describe("canConfigure", (): void => {  // Aquí cambiaste a "canConfigure"
+describe("canReconfigure", (): void => {  // Aquí cambiaste a "canReconfigure"
   it("should be a function", (): void => {
-    expect(canConfigure).toBeTypeOf("function");
+    expect(canReconfigure).toBeTypeOf("function");
   });
 
   it("should throw if first parameter is missing", (): void => {
-    expect((): void => canConfigure()).toThrow();
+    expect((): void => canReconfigure()).toThrow();
   });
 
   it("should throw if first parameter is not a string", (): void => {
-    expect((): void => canConfigure(2)).toThrow();
+    expect((): void => canReconfigure(2)).toThrow();
   });
 
   it("should throw if second parameter is not a string", (): void => {
-    expect((): void => canConfigure("a")).toThrow();
+    expect((): void => canReconfigure("a")).toThrow();
   });
+
+  it('should return a boolean', (): void => {
+    expect(canReconfigure('a','b')).toBeTypeOf('boolean')
+  })
+
+  it('should return false if strings have different lengths', (): void => {
+    expect(canReconfigure('abc','de')).toBe(false)
+  })
 });
 
